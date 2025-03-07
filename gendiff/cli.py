a009fb8105a26347ce762a72847c188a41a09671
Artem Sharovatov <git@sharovatov.me>
2025-03-07 16:53:59 +0300

    Add format option to CLI

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -9,5 +9,10 @@ def cli():
     )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
+    parser.add_argument(
+        "-f", "--format",
+        type=str,
+        help="set format of output"
+    )
     arguments = parser.parse_args()
     print(arguments)
