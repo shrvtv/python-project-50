53b3385c2434d6bdabe25bdaeb4a37be33efb284
Artem Sharovatov <git@sharovatov.me>
2025-03-13 22:49:26 +0300

    Simplify testing process

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,5 +1,3 @@
-import textwrap
-
 from gendiff import generate_diff
 
 
@@ -7,13 +5,5 @@ def test_generate_diff():
     assert generate_diff(
         "tests/test_data/file1.json",
         "tests/test_data/file2.json"
-    ) == textwrap.dedent('''\
-        {
-          - follow: false
-            host: hexlet.io
-          - proxy: 123.234.53.22
-          - timeout: 50
-          + timeout: 20
-          + verbose: true
-        }
-    ''')
+    ) == open("tests/test_data/file1_vs_file2.txt").read()
+
