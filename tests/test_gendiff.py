7312820cc1fadf75b7c84bd7a5e8822bc86132c8
Artem Sharovatov <git@sharovatov.me>
2025-03-13 23:07:33 +0300

    Add new test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,5 +5,12 @@ def test_generate_diff():
     assert generate_diff(
         "tests/test_data/file1.json",
         "tests/test_data/file2.json"
-    ) == open("tests/test_data/file1_vs_file2.txt").read()
+    ) == open("tests/test_data/results/file1_vs_file2.txt").read()
 
+
+def test_generate_diff_reversed():
+    # Output depends on file order
+    assert generate_diff(
+        "tests/test_data/file2.json",
+        "tests/test_data/file1.json"
+    ) == open("tests/test_data/results/file2_vs_file1.txt").read()
