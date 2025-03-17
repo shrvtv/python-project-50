fd377386f8b00400dd6699103ded57d758214aac
Artem Sharovatov <git@sharovatov.me>
2025-03-17 22:00:18 +0300

    Refactor and simplify gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -5,6 +5,21 @@ import gendiff.parser
 MISSING = object()
 
 
+def compare(key, value1, value2):
+    if value1 is not MISSING and value2 is MISSING:
+        return f"  - {key}: {value1}\n"
+    elif value1 is MISSING and value2 is not MISSING:
+        return f"  + {key}: {value2}\n"
+    else:  # both values exist
+        if value1 == value2:
+            return f"    {key}: {value1}\n"
+        else:
+            return (
+                f"  - {key}: {value1}\n"
+              + f"  + {key}: {value2}\n"
+            )    
+
+
 def generate_diff(file_path1, file_path2):
     file1 = gendiff.parser.read_file(Path(file_path1))
     file2 = gendiff.parser.read_file(Path(file_path2))
@@ -13,19 +28,8 @@ def generate_diff(file_path1, file_path2):
     all_keys_sorted = sorted(
         keys1 + [key for key in file2.keys() if key not in keys1]
     )
-    diff = ""
-
-    for key in all_keys_sorted:
-        value1 = file1.get(key, MISSING)
-        value2 = file2.get(key, MISSING)
-        if value1 is not MISSING and value2 is MISSING:
-            diff += f"  - {key}: {value1}\n"
-        elif value1 is MISSING and value2 is not MISSING:
-            diff += f"  + {key}: {value2}\n"
-        else:  # both values exist
-            if value1 == value2:
-                diff += f"    {key}: {value1}\n"
-            else:
-                diff += f"  - {key}: {value1}\n"
-                diff += f"  + {key}: {value2}\n"
-    return "{\n" + diff + "}"  # diff lines end with \n
+    comparisons = [
+        compare(key, file1.get(key, MISSING), file2.get(key, MISSING))
+        for key in all_keys_sorted
+    ]
+    return ''.join(["{\n", *comparisons, "}"])  # diff lines end with \n
