ce72a9cc66709d3ed7f3d28be4d4eca3473ce890
Artem Sharovatov <git@sharovatov.me>
2025-03-17 22:26:05 +0300

    Refactor gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -6,14 +6,14 @@ MISSING = object()
 
 
 def compare(key, value1, value2):
-    if value1 is not MISSING and value2 is MISSING:
-        return f"  - {key}: {value1}\n"
-    elif value1 is MISSING and value2 is not MISSING:
+    if value1 is MISSING:     # new key
         return f"  + {key}: {value2}\n"
-    else:  # both values exist
-        if value1 == value2:
+    elif value2 is MISSING:   # key removed
+        return f"  - {key}: {value1}\n"
+    else:                     # both values exist
+        if value1 == value2:  # value didn't change
             return f"    {key}: {value1}\n"
-        else:
+        else:                 # value has been changed
             return (
                 f"  - {key}: {value1}\n"
               + f"  + {key}: {value2}\n"
@@ -24,12 +24,12 @@ def generate_diff(file_path1, file_path2):
     file1 = gendiff.parser.read_file(Path(file_path1))
     file2 = gendiff.parser.read_file(Path(file_path2))
 
-    keys1 = list(file1.keys())
-    all_keys_sorted = sorted(
-        keys1 + [key for key in file2.keys() if key not in keys1]
-    )
+    # merging and sorting all keys
+    all_keys_sorted = sorted(file1.keys() | file2.keys())
+
     comparisons = [
         compare(key, file1.get(key, MISSING), file2.get(key, MISSING))
         for key in all_keys_sorted
     ]
-    return ''.join(["{\n", *comparisons, "}"])  # diff lines end with \n
+
+    return ''.join(["{\n", *comparisons, "}"])
