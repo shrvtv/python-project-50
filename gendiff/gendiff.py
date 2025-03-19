34ecedd00e856f80b51ab91557e498004d2cadfe
Artem Sharovatov <git@sharovatov.me>
2025-03-19 20:49:16 +0300

    Refactor functions to add initial support for recursive checks in nested dictionaries

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -12,6 +12,9 @@ COMPARISON = {
 
 
 def generate_comparison_line(key, value1, value2, indentation_level=0):
+    """Generate a comparison output with the correct indentation.
+    Returns a list of lines if given 2 dicts, otherwise return a single line
+    """
     indentation = indentation_level * INDENT
 
     def make_line(comparison_result, value):
@@ -24,20 +27,20 @@ def generate_comparison_line(key, value1, value2, indentation_level=0):
     elif value1 == value2:
         return make_line("no change", value1)
     else:  # value has changed
-        return (
-            make_line("removed", value1)
-            + make_line("added", value2)
-        )
+        if isinstance(value1, dict) and isinstance(value2, dict):
+            return stylish(value1, value2)
+        else:
+            return (
+                make_line("removed", value1)
+                + make_line("added", value2)
+            )
 
 
-def stylish(all_keys_sorted, parsed_file1, parsed_file2):
+def stylish(dict1, dict2):
     comparisons = [
-        generate_comparison_line(
-            key,
-            parsed_file1.get(key),
-            parsed_file2.get(key)
-        )
-        for key in all_keys_sorted
+        generate_comparison_line(key, dict1.get(key), dict2.get(key))
+        # merging and sorting all keys before iterating over them
+        for key in sorted(dict1.keys() | dict2.keys())
     ]
     return ''.join(["{\n", *comparisons, "}"])
 
@@ -46,11 +49,8 @@ def generate_diff(file_path1, file_path2, format_name="stylish"):
     file1 = gendiff.parser.read_file(Path(file_path1))
     file2 = gendiff.parser.read_file(Path(file_path2))
 
-    # merging and sorting all keys
-    all_keys_sorted = sorted(file1.keys() | file2.keys())
-
     match format_name:
         case "stylish":
-            return stylish(all_keys_sorted, file1, file2)
+            return stylish(file1, file2)
         case _:
             raise ValueError("unknown output format")
