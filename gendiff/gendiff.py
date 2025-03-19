f59ec94a1efa51b7ec9ec5efc117293427b318ac
Artem Sharovatov <git@sharovatov.me>
2025-03-19 16:09:19 +0300

    Refactor 'compare' function to comply with both DRY and KISS principles

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,29 +3,39 @@ from pathlib import Path
 import gendiff.parser
 
 MISSING = object()
+INDENT = "    "
+COMPARISON = {
+    "no change": "    ",
+        "added": "  + ",
+      "removed": "  - "
+}
 
 
-def compare(key, value1, value2):
-    if value1 is MISSING:     # new key
-        return f"  + {key}: {value2}\n"
-    elif value2 is MISSING:   # key removed
-        return f"  - {key}: {value1}\n"
-    else:                     # both values exist
-        if value1 == value2:  # value didn't change
-            return f"    {key}: {value1}\n"
-        else:                 # value has been changed
-            return (
-                f"  - {key}: {value1}\n"
-              + f"  + {key}: {value2}\n"
-            )    
+def compare(key, value1, value2, indentation_level=0):
+    indentation = indentation_level * INDENT
+
+    def make_comparison_line(comparison_result, value):
+        return f"{indentation}{COMPARISON[comparison_result]}{key}: {value}\n"
+
+    if value1 is None:
+        return make_comparison_line("added", value2)
+    elif value2 is None:
+        return make_comparison_line("removed", value1)
+    elif value1 == value2:
+        return make_comparison_line("no change", value1)
+    else:  # value has changed
+        return (
+            make_comparison_line("removed", value1)
+            + make_comparison_line("added", value2)
+        )
 
 
 def stylish(all_keys_sorted, parsed_file1, parsed_file2):
     comparisons = [
         compare(
             key,
-            parsed_file1.get(key, MISSING),
-            parsed_file2.get(key, MISSING)
+            parsed_file1.get(key),
+            parsed_file2.get(key)
         )
         for key in all_keys_sorted
     ]
