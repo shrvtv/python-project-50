7ad79454e29fef3e2b954a9dd47606eafe58aa8e
Artem Sharovatov <git@sharovatov.me>
2025-03-19 13:00:36 +0300

    Rename files with test data

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,29 +3,29 @@ from gendiff import generate_diff
 
 def test_generate_diff_JSON():
     assert generate_diff(  # JSON
-        "tests/test_data/file1.json",
-        "tests/test_data/file2.json"
-    ) == open("tests/test_data/results/file1_vs_file2.json").read()
+        "tests/test_data/flat1.json",
+        "tests/test_data/flat2.json"
+    ) == open("tests/test_data/results/flat1_vs flat2.txt").read()
 
 
 def test_generate_diff_YAML():
     assert generate_diff(  # YAML
-        "tests/test_data/file1.yml",
-        "tests/test_data/file2.yml"
-    ) == open("tests/test_data/results/file1_vs_file2.json").read()
+        "tests/test_data/flat1.yml",
+        "tests/test_data/flat2.yml"
+    ) == open("tests/test_data/results/flat1_vs flat2.txt").read()
 
 
 # Output depends on file order
 def test_generate_diff_JSON_reversed():
     assert generate_diff(  # JSON
-        "tests/test_data/file2.json",
-        "tests/test_data/file1.json"
-    ) == open("tests/test_data/results/file2_vs_file1.json").read()
+        "tests/test_data/flat2.json",
+        "tests/test_data/flat1.json"
+    ) == open("tests/test_data/results/flat2_vs_flat1.txt").read()
 
 
 # Output depends on file order
 def test_generate_diff_YAML_reversed():
     assert generate_diff(  # YAML
-        "tests/test_data/file2.yml",
-        "tests/test_data/file1.yml"
-    ) == open("tests/test_data/results/file2_vs_file1.json").read()
+        "tests/test_data/flat2.yml",
+        "tests/test_data/flat1.yml"
+    ) == open("tests/test_data/results/flat2_vs_flat1.txt").read()
