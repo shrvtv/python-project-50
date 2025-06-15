26e7cac07ff3c24a60d8ad2665fa02554431b597
Artem Sharovatov <git@sharovatov.me>
2025-06-15 15:38:39 +0300

    Move test data

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,6 +3,6 @@ from gendiff.gendiff import generate_diff
 
 def test_generate_diff_flat_json():
     assert generate_diff(
-        'tests/test_data/file1.json',
-        'tests/test_data/file2.json'
-    ) == open('tests/test_data/file1_vs_file2').read()
+        'tests/test_data/flat/json/file1.json',
+        'tests/test_data/flat/json/file2.json'
+    ) == open('tests/test_data/results/flat').read()
