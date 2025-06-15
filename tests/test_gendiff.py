9f6ecde879cefd3df2faef80dbfadb3e48d6fdd8
Artem Sharovatov <git@sharovatov.me>
2025-06-15 15:41:27 +0300

    Add new tests for YAML files

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,3 +6,17 @@ def test_generate_diff_flat_json():
         'tests/test_data/flat/json/file1.json',
         'tests/test_data/flat/json/file2.json'
     ) == open('tests/test_data/results/flat').read()
+
+
+def test_generate_diff_flat_yaml():
+    assert generate_diff(
+        'tests/test_data/flat/yaml/file1.yml',
+        'tests/test_data/flat/yaml/file2.yaml'
+    ) == open('tests/test_data/results/flat').read()
+
+
+def test_generate_diff_flat_mixed_format():
+    assert generate_diff(
+        'tests/test_data/flat/json/file1.json',
+        'tests/test_data/flat/yaml/file2.yaml'
+    ) == open('tests/test_data/results/flat').read()
