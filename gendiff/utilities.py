1fdb8317dfe424369c7a946bf7feb054ec8c85db
Artem Sharovatov <git@sharovatov.me>
2025-06-16 20:02:02 +0300

    Fix singleton protection

--- gendiff/utilities.py
+++ gendiff/utilities.py
@@ -2,8 +2,12 @@ def make_line(change_type, key, value, level=0):
     return f"{level * '    '}{change_type}{key}: {value}"
 
 
-def mimic_json(text):
-    text = text.replace('True', 'true')
-    text = text.replace('False', 'false')
-    text = text.replace('None', 'null')
-    return text
+def protect_singleton(text):
+    match text:
+        case True:
+            return 'true'
+        case False:
+            return 'false'
+        case None:
+            return 'null'
+    raise ValueError('Not a singleton')
