05fdfc7316e22696c89dac064ecee5dd6224d8e3
Artem Sharovatov <git@sharovatov.me>
2025-06-16 13:48:09 +0300

    Separate element comparison logic into a separate function

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -4,6 +4,8 @@ import os
 
 import yaml
 
+EXCEPTIONS = (True, False)
+
 CHANGE_SYNTAX = {
     'added': '  + ',
     'removed': '  - ',
@@ -34,6 +36,34 @@ def mimic_json(text):
     return text
 
 
+def compare(first, second):
+    if first in EXCEPTIONS:
+        first = str(first)
+    if second in EXCEPTIONS:
+        second = str(second)
+
+    if second and not first:
+        change_type = CHANGE_SYNTAX['added']
+        value = second
+    elif first and not second:
+        change_type = CHANGE_SYNTAX['removed']
+        value = first
+    else:  # key is present in both files
+        if first == second:
+            change_type = CHANGE_SYNTAX['unchanged']
+            value = first
+        else:
+            change_type = CHANGE_SYNTAX['changed']
+            value = {
+                'old': first,
+                'new': second
+            }
+    return {
+        'change_type': change_type,
+        'value': value
+    }
+
+
 def generate_diff(file1, file2):
     first = parse(file1)
     second = parse(file2)
@@ -42,27 +72,7 @@ def generate_diff(file1, file2):
     second_keys = set(second.keys())
     for key in first_keys.union(second_keys):
         first_value, second_value = first.get(key), second.get(key)
-
-        if key not in first_keys and key in second_keys:
-            change_type = CHANGE_SYNTAX['added']
-            value = second_value
-        elif key in first_keys and key not in second_keys:
-            change_type = CHANGE_SYNTAX['removed']
-            value = first_value
-        else:  # key is present in both files
-            if first_value == second_value:
-                change_type = CHANGE_SYNTAX['unchanged']
-                value = first_value
-            else:
-                change_type = CHANGE_SYNTAX['changed']
-                value = {
-                    'old': first_value,
-                    'new': second_value
-                }
-        comparison[key] = {
-            'change_type': change_type,
-            'value': value
-        }
+        comparison[key] = compare(first_value, second_value)
 
     lines = []
     for key in sorted(comparison.keys()):
