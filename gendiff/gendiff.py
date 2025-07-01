633fc7104663e7b1e1829d61063c89a9a0cc38f3
Artem Sharovatov <git@sharovatov.me>
2025-07-01 15:14:02 +0300

    Fix args position bug

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -123,7 +123,7 @@ def main():
 
     args = parser.parse_args()
 
-    print(generate_diff(args.format, args.first_file, args.second_file))
+    print(generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == "__main__":
