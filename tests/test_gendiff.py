a259aa9d7697846e7d641e7e1f49e571d15c58ee
Artem Sharovatov <git@sharovatov.me>
2025-07-01 14:35:26 +0300

    Fix function signature error

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,56 +3,56 @@ from gendiff.gendiff import generate_diff
 
 def test_generate_diff_flat_json():
     assert generate_diff(
-        'stylish',
         'tests/test_data/flat/json/file1.json',
-        'tests/test_data/flat/json/file2.json'
+        'tests/test_data/flat/json/file2.json',
+        'stylish'
     ) == open('tests/test_data/results/flat').read()
 
 
 def test_generate_diff_flat_yaml():
     assert generate_diff(
-        'stylish',
         'tests/test_data/flat/yaml/file1.yml',
-        'tests/test_data/flat/yaml/file2.yaml'
+        'tests/test_data/flat/yaml/file2.yaml',
+        'stylish'
     ) == open('tests/test_data/results/flat').read()
 
 
 def test_generate_diff_flat_mixed():
     assert generate_diff(
-        'stylish',
         'tests/test_data/flat/json/file1.json',
-        'tests/test_data/flat/yaml/file2.yaml'
+        'tests/test_data/flat/yaml/file2.yaml',
+        'stylish'
     ) == open('tests/test_data/results/flat').read()
 
 
 def test_generate_diff_recursive_json():
     assert generate_diff(
-        'stylish',
         'tests/test_data/recursive/json/file1.json',
-        'tests/test_data/recursive/json/file2.json'
+        'tests/test_data/recursive/json/file2.json',
+        'stylish'
     ) == open('tests/test_data/results/stylish_recursive').read()
 
 
 def test_generate_diff_recursive_yaml():
     assert generate_diff(
-        'stylish',
         'tests/test_data/recursive/yaml/file1.yml',
-        'tests/test_data/recursive/yaml/file2.yaml'
+        'tests/test_data/recursive/yaml/file2.yaml',
+        'stylish'
     ) == open('tests/test_data/results/stylish_recursive').read()
 
 
 def test_generate_diff_recursive_mixed():
     assert generate_diff(
-        'stylish',
         'tests/test_data/recursive/json/file1.json',
-        'tests/test_data/recursive/yaml/file2.yaml'
+        'tests/test_data/recursive/yaml/file2.yaml',
+        'stylish'
     ) == open('tests/test_data/results/stylish_recursive').read()
 
 
 def test_generate_diff_plain_mixed():
     assert generate_diff(
-        'plain',
         'tests/test_data/recursive/json/file1.json',
-        'tests/test_data/recursive/yaml/file2.yaml'
+        'tests/test_data/recursive/yaml/file2.yaml',
+        'plain'
     ) == open('tests/test_data/results/plain_recursive').read()
 
